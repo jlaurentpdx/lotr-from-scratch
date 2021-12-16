ad0dfebc2f3e42e6b6bd47ce16cfe4e0e7b74a9f
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-12-16 15:42:10 -0800

    added fetch call to Films

--- src/views/Films/Films.js
+++ src/views/Films/Films.js
@@ -1,5 +1,22 @@
+import { useState, useEffect } from 'react';
+import { fetchFilms } from '../../services/films';
 import './Films.css';
 
 export default function Films() {
-  return <div>Films page</div>;
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchFilms();
+      setFilms(data);
+      // console.log(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="films-page">
+      <h2>This is the films page.</h2>
+    </div>
+  );
 }
