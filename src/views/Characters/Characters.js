8cd8f8746b1a943de61522946c181ecdcf2505d5
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-07 15:59:10 -0800

    characters rendering to page

--- src/views/Characters/Characters.js
+++ src/views/Characters/Characters.js
@@ -1,5 +1,41 @@
+import { useState, useEffect } from 'react';
+import { fetchCharacters } from '../../services/characters';
 import './Characters.css';
 
 export default function Characters() {
-  return <div>Characters page</div>;
+  const [characters, setCharacters] = useState([]);
+  const [query, setQuery] = useState('');
+  const [race, setRace] = useState('All');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchCharacters(race, query);
+      setCharacters(data);
+    };
+    fetchData();
+  }, [race, query]);
+
+  return (
+    <div>
+      <h2>Characters</h2>
+      <input
+        value={query}
+        type="text"
+        placeholder="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <select value={race} onChange={(e) => setRace(e.target.value)}>
+        <option value="All">All</option>
+        <option value="Dwarf">Dwarf</option>
+        <option value="Elf">Elf</option>
+        <option value="Hobbit">Hobbit</option>
+        <option value="Human">Human</option>
+        <option value="Maiar">Maiar</option>
+        <option value="Orc">Orc</option>
+      </select>
+      {characters.map((character) => (
+        <h3 key={character.id}>{character.name}</h3>
+      ))}
+    </div>
+  );
 }
